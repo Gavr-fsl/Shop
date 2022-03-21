d0ffce3d1699eb2a2f1a396e1c99053faefda7a1
unknown <pgavr11@yandex.ru>
2022-03-21 10:27:58 +0300

    first commit

--- resources/views/users/cabinet.blade.php
+++ resources/views/users/cabinet.blade.php
@@ -15,6 +15,7 @@
                     <li class="list-group-item">Почта: {{ Auth::user()->email }}</li>
                     <li class="list-group-item">Логин: {{ Auth::user()->login }}</li>
                     <li class="list-group-item">Адрес: {{ Auth::user()->address }}</li>
+                    <li class="list-group-item">Адрес: {{ Auth::user()->address }}</li>
                 </ul>
                 <a href="{{ route('cabinetEdit') }}" class="btn btn-outline-primary mt-4">Редактировать аккаунт</a>
             </div>
